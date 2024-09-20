942be8709d651bc11a697a955c52ff8a87eb7646
Pol Castillo <rpcastilloy@gmail.com>
2024-09-19 20:55:54 -0500

    first project with js

---
+++ 06-project-js-html-css/01-project/assets/js/main.js
@@ -0,0 +1,9 @@
+let names = "Pol Roger";
+let lastname = "Castillo";
+let age = 43;
+let country = "Peru";
+let knowledge =  ["Java", "React", "Html", "Css", "Javascript"];
+age = 45;
+knowledge.push("Ingles");
+
+console.log(names, lastname, age, country, knowledge);
